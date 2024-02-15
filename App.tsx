272272b569d59e73589d93367cbe9e20d7da71ec
Prem Yathvik <90135427+PremRockz@users.noreply.github.com>
2024-02-15 17:38:15 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  Text,
+  View, } from 'react-native';
+import Home from './src/component/Home/home';
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+import LocalNotification from './localNotification';
+import RemoteNotification from './RemoteNotification';
+
+const App = () => {
+  const isDarkMode =true
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={{alignItems:'center',justifyContent:'center',flex:1,alignContent:'center'}}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView>
+        <View >
+        <RemoteNotification />
+          <Text> Push Notification!! </Text>
+          <Button title={'Click Here'} onPress={LocalNotification} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
